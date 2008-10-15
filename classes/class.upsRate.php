3f5ba74559c39ddd7c5bccd12b637162acdcf8a7
marcrosoft <marcrosoft@2962a8ba-8c0a-11dd-aa02-45bbecb07bf0>
2008-10-15 04:10:57 +0000

    Added a variable to check if access function has been called.

--- classes/class.upsRate.php
+++ classes/class.upsRate.php
@@ -12,7 +12,7 @@ class upsRate {
 		$content = $this->ups->sandwich($this->ups->templatePath.'Rates/RatingServiceSelection_Request.xml', array(), array());
 		$content .=$this->ups-sandwich($this->ups->templatePath.'Rates/RatingServiceSelection_PickupType.xml', array('{PICKUP_TYPE}'), array($pickupType));
 		$xml .= $this->ups->sandwich($this->ups->templatePath.'Rates/RatingServiceSelection_Main.xml', array('{CONTENT}'), array($content));
-		$responseXML = $this->ups->request('Track', $xml);
+		$responseXML = $this->ups->request('Rate', $xml);
 		$xmlParser = new XML2Array();
 		$fromUPS = $xmlParser->parse($responseXML);
 		return $fromUPS;
