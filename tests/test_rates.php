bd4ae3808f733286d1bfb018b25a054d18fcb186
marcrosoft <marcrosoft@2962a8ba-8c0a-11dd-aa02-45bbecb07bf0>
2008-10-23 04:11:53 +0000

    Fixed some code indention and a "}" mistake in ups.php Added a form to test_rates.php for easier testing without exposing accesskeys

--- tests/test_rates.php
+++ tests/test_rates.php
@@ -1,20 +1,30 @@
 <?php
 //TODO: This is not a functional test yet as class.upsRate.php is not finished
 require('../classes/class.ups.php');
-require('../classes/ckass.upsRate.php');
+require('../classes/class.upsRate.php');
 
-// Replace the values below with your own credentials.
-$upsConnect = new ups('accessNumber','username','password');
-$upsConnect->setTemplatePath('../xml/');
-$upsConnect->setTestingMode(1); // Change this to 0 for production
+$accessNumber = $_POST['accessNumber'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$upsRate = new upsRate($upsConect);
-
-print '<h2>UPS Rates Test</h2>';
-
-$trackingData = $upsRate->rate();
-
-
-
-
+if (isset($accessNumber)) {
+	// Replace the values below with your own credentials.
+	$upsConnect = new ups("$accessNumber","$username","$password");
+	$upsConnect->setTemplatePath('../xml/');
+	$upsConnect->setTestingMode(1); // Change this to 0 for production
+	
+	$upsRate = new upsRate($upsConnect);
+	
+	print '<h2>UPS Rates Test</h2>';
+	
+	$rateData = $upsRate->rate();
+	echo '<pre>'; print_r($rateData); echo '</pre>';
+	
+}
 ?>
+<form action="" method="POST">
+	Access Key: <input type="text" name="accessNumber" value="<?php echo $accessNumber; ?>" /><br />
+	Username: <input type="text" name="username" value="<?php echo $username; ?>" /><br />
+	Password: <input type="password" name="password" value="<?php echo $password; ?>" /><br />
+	<input type="submit" name="submit" /><br />
+</form>
