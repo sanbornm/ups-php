b2c46fdb1f80962a3cb5ab256385c8f849e8f2a0
marcrosoft <marcrosoft@2962a8ba-8c0a-11dd-aa02-45bbecb07bf0>
2008-10-29 03:29:23 +0000

    - Added a function to return just the rate - Added a function to return the entire xml reponse in an array - Added a better xml parser to class.ups.php, It puts xml elements in an array while preserving the original xml tag name.   It also keeps the orginal xml structure intact to avoid confusion

--- classes/class.upsRate.php
+++ classes/class.upsRate.php
@@ -10,6 +10,7 @@ class upsRate {
 	var $packageWeightXML;	
 
 	var $xmlSent;
+	var $rateResponse;
 	
 	function upsRate($upsObj) {
 		// Must pass the UPS object to this class for it to work
@@ -17,7 +18,7 @@ class upsRate {
 	}
 	
 	// Main function that puts together all the XML builder function variables.  Builds the final XML for Rate calculation
-	function rate() {
+	function sendRateRequest() {
 		// First part of XML is the access part,
 		$xml = $this->ups->access();
 		$content = $this->requestXML;
@@ -27,11 +28,17 @@ class upsRate {
 		
 		$xml .= $this->ups->sandwich($this->ups->templatePath.'Rates/RatingServiceSelection_Main.xml', array('{CONTENT}'), array($content));
 
+		# Put the xml send to UPS into a variable so we can call it later for debugging purposes
 		$this->xmlSent = $xml;
 
 		$responseXML = $this->ups->request('Rate', $xml);
-		$xmlParser = new XML2Array();
-		$fromUPS = $xmlParser->parse($responseXML);
+		#$xmlParser = new XML2Array();
+		#$fromUPS = $xmlParser->parse($responseXML);
+		$xmlParser = new upsxmlParser();
+		$fromUPS = $xmlParser->xmlparser($responseXML);
+		$fromUPS = $xmlParser->getData();
+
+		$this->rateResponse = $fromUPS;
 		return $fromUPS;
 	}
 
@@ -118,6 +125,18 @@ class upsRate {
 
 		$this->packageWeightXML = $packageWeight;
 		return $packageWeight;
+	}
+
+	// Output the entire array of XML returned by UPS
+	function returnResponseArray() {
+		$rateResponse = $this->rateResponse;
+		return $rateResponse;
+	}
+
+	// Return the total monitary value of the service
+	function returnRate() {
+		$rateResponse = $this->rateResponse;
+		return $rateResponse['RatingServiceSelectionResponse']['RatedShipment']['TotalCharges']['MonetaryValue']['VALUE'];
 	}
 
 		
