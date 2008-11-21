993551edc7666b45a9d42b6e621e474a2e1818b4
marcrosoft <marcrosoft@2962a8ba-8c0a-11dd-aa02-45bbecb07bf0>
2008-11-21 07:15:56 +0000

    Added individual tracking information to UPS test_tracking.php

--- tests/tracking/test_tracking.php
+++ tests/tracking/test_tracking.php
@@ -13,18 +13,40 @@ $trackingNumber = $_POST['tracking'];
 // Obviously, replace the values below with your setup.
 if ($accessNumber != '' && $username != '' && $password != '') {
 	$ups_connect = new ups($accessNumber,$username,$password);
-	$ups_connect->setTemplatePath('../../xml/');
-	$ups_connect->setTestingMode(0); //Change this to 0 for production
+$ups_connect->setTemplatePath('../../xml/');
+$ups_connect->setTestingMode(1); //Change this to 0 for production
 
 	$upsTrack = new upsTrack($ups_connect);
 
 	$tracking_data = $upsTrack->track($trackingNumber);
+	$response = $upsTrack->returnResponseArray();
 ?>
-<h2>UPS Tracking Test</h2>
+
+<h2>Tracking Information for <?php echo $trackingNumber; ?></h2>
+<dl>
+	<dt>Status</dt>
+		<dd><?php echo $response['TrackResponse']['Shipment']['Package']['Activity']['Status']['StatusType']['Description']['VALUE']; ?></dd> 	
+	<dt>Shipper Number:</dt>
+		<dd><?php echo $response['TrackResponse']['Shipment']['Shipper']['ShipperNumber']['VALUE']; ?></dd> 
+	<dt>Ship to Address:</dt>
+		<dd><?php echo $response['TrackResponse']['Shipment']['ShipTo']['Address']['AddressLine1']['VALUE']; ?></dd> 	
+		<dd><?php echo $response['TrackResponse']['Shipment']['ShipTo']['Address']['AddressLine2']['VALUE']; ?></dd> 	
+		<dd><?php echo $response['TrackResponse']['Shipment']['ShipTo']['Address']['City']['VALUE']; ?></dd> 	
+		<dd><?php echo $response['TrackResponse']['Shipment']['ShipTo']['Address']['StateProvinceCode']['VALUE']; ?></dd> 	
+		<dd><?php echo $response['TrackResponse']['Shipment']['ShipTo']['Address']['PostalCode']['VALUE']; ?></dd> 	
+		<dd><?php echo $response['TrackResponse']['Shipment']['ShipTo']['Address']['CountryCode']['VALUE']; ?></dd> 	
+	<dt>Service</dt>
+		<dd><?php echo $response['TrackResponse']['Shipment']['Service']['Code']['VALUE']; ?></dd> 	
+		<dd><?php echo $response['TrackResponse']['Shipment']['Service']['Description']['VALUE']; ?></dd> 	
+				
+	
+</dl>
+<h2>UPS Tracking Response Array</h2>
 <pre><?php print_r($upsTrack->returnResponseArray()); ?></pre>
 
 <h2>XML Sent to UPS</h2>
 <pre><?php echo htmlspecialchars($upsTrack->xmlSent); ?></pre>
+
 <?php } else { ?>
 One or more parts of the form are not filled out.  You must provide your UPS credentials in order to get an accurate rate.
 <?php } ?>
